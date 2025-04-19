e7c395fda44ef23919c1372ea127f8989433989c
Sylvia <162008385+CylciaUwU@users.noreply.github.com>
2025-04-19 23:00:30 +0700

    Create _ROCK_FRUIT_WORLD2NPC.lua

---
+++ _ROCK_FRUIT_WORLD2NPC.lua
@@ -0,0 +1,20 @@
+local WORLD2 = {
+	workspace.Mob.Anubis,
+	workspace.Mob["Bacon Blade"],
+	workspace.Mob["Bacon Candy"],
+	workspace.Mob["Bacon Cream"],
+	workspace.Mob["Bacon Haki"],
+	workspace.Mob["Bacon Mummy"],
+	workspace.Mob["Bacon Music"],
+	workspace.Mob["Bacon Quincy"],
+	workspace.Mob["Bacon Reaper"],
+	workspace.Mob["Bacon Sigma"],
+	workspace.Mob["Bacon Skibidi"],
+	workspace.Mob["Bacon Sunflower"],
+	workspace.Mob["Grass Man"],
+	workspace.Mob["Horse Man"],
+	workspace.Mob["Shark Monster"],
+	workspace.Mob["Sunflower Man"],
+	workspace.Mob["Witch Cult"]
+}
+return WORLD2
