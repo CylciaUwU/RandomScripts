2cdc14ccec1b877fe962bccd02fbbb7a3cacaeea
Sylvia <162008385+CylciaUwU@users.noreply.github.com>
2025-04-24 15:14:25 +0700

    Update Fluent

--- rock fruit.lua
+++ rock fruit.lua
@@ -294,7 +294,7 @@ function ClientUtils:AutoEquip(Value)
 end
 
 local Fluent
-Fluent = loadstring(game:HttpGet("https://gist.github.com/CylciaUwU/4ce60ba116cb52855f282a7f50b1866b/raw/f7f2f59272145614852f28d5659e1b7e4223518a/Fluent_Edited.luau"))();
+Fluent = loadstring(game:HttpGet("https://gist.githubusercontent.com/CylciaUwU/4ce60ba116cb52855f282a7f50b1866b/raw/46a055c61eb48d2ab990923121f81f57cf15c04c/Fluent_Edited.luau"))();
 SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 local Options = Fluent.Options
